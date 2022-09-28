6977c2605cabd2144f12ba0382cf41b292edf213
MichaelSinn <michaelsinn.contact@gmail.com>
2022-09-28 12:00:41 -0400

    Added Google Maps to the main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,32 @@
-console.log(API_KEY);
-const form = document.querySelector('#submit-btn')
-const weather = document.querySelector('#filter-weather')
-const location = document.querySelector('#filter-location')
+const form = document.querySelector('#submit-btn');
+const weather = document.querySelector('#filter-weather');
+const locationEl = document.querySelector('#filter-location');
+const googleApiScript = $("#maps-api");
+let scriptSrc = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap&v=weekly`;
+googleApiScript.attr("src", scriptSrc);
+
 form.addEventListener('click', function(e) {
     e.preventDefault();
     const weatherData = weather.value;
-    const locationData = location.value;
+    const locationData = locationEl.value;
     console.log(weatherData);
     console.log(locationData);
-})
+});
 
-let googleApiSrc = $("#maps-api");
-let script_src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap&v=weekly`;
-localStorage.getItem('selected-sites')
+let map;
+let script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+script.async = true;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
+window.initMap = initMap;
+
+document.head.appendChild(script);
+
+localStorage.getItem('selected-sites');
